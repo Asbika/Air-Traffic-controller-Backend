e8410cbe5e7ba52b5a5046f18962f1b95b0958d7
Asbika <hicham.asbika.e@gmail.com>
2022-10-23 00:46:18 +0200

    Initial commit

---
+++ src/main/java/com/example/airtrafficcontrolbackend/AirTrafficControlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.airtrafficcontrolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirTrafficControlBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirTrafficControlBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/airtrafficcontrolbackend/AirTrafficControlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airtrafficcontrolbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirTrafficControlBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
